68b90283a5e559af75fa1130a42d513acf210124
atillman34817 <adrian.tillman@gmail.com>
2019-04-15 16:44:24 -0400

    Adding Test Skeleton for ReqeustServiceHandler

---
+++ LegalTrucking.Tests.Unit/Services/Handlers/Services/RequestServiceCommandHandlerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LegalTrucking.Tests.Unit.Services.Handlers.Services
+{
+    public class RequestServiceCommandHandlerTest
+    {
+        [Fact(DisplayName = "A request service command should result in creation of a new service request")]
+        public void WhenARequestServiceCommandIsReceived()
+        {
+
+        }
+    }
+}
