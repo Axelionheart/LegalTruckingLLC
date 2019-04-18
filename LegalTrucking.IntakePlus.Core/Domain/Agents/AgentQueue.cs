76c6fb8971e82bef9ac5ed065f7f957deec48295
atillman34817 <adrian.tillman@gmail.com>
2019-04-18 13:07:24 -0400

    Adding AgentQueue

---
+++ LegalTrucking.IntakePlus.Core/Domain/Agents/AgentQueue.cs
@@ -0,0 +1,29 @@
+﻿using LegalTrucking.IntakePlus.Core.Domain.Common.CircularBuffer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegalTrucking.IntakePlus.Core.Domain.Agents
+{
+    public class AgentQueue
+    {
+        private CircularBuffer<Agent> _buffer;
+
+        public AgentQueue()
+        {
+            _buffer = new CircularBuffer<Agent>(10);
+        }
+
+        public void AddAn(Agent agent)
+        {
+            _buffer.PushBack(agent);
+        }
+
+        public bool Contains(Agent agent)
+        {
+            return _buffer.Contains(agent);
+        }
+    }
+}
---
+++ LegalTrucking.Tests.Unit/Domain/Agents/AgentQueueTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LegalTrucking.IntakePlus.Core.Domain.Agents;
+
+using Machine.Specifications;
+
+namespace LegalTrucking.Tests.Unit.Domain.Agents
+{
+    public class When_we_add_an_agent_they_get_added_to_the_queue
+    {
+        static AgentQueue _agentQueue;
+        static Agent agent = new Agent("Adrian", "Tillman");
+        static bool doesContain = false;
+
+        Establish context = () =>
+        {
+            _agentQueue = new AgentQueue();
+        };
+
+        Because of = () => _agentQueue.AddAn(agent);
+
+        It should_contain_that_agent = () => _agentQueue.Contains(agent).ShouldEqual(true);
+    }
+}
