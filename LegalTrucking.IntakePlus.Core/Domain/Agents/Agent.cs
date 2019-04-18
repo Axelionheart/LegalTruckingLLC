f53bb3cb96cec7b532a7719c500dee188ddc2b60
atillman34817 <adrian.tillman@gmail.com>
2019-04-18 11:47:16 -0400

    Adding Agent Class

---
+++ LegalTrucking.IntakePlus.Core/Domain/Agents/Agent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LegalTrucking.IntakePlus.Core.Domain.Common;
+
+namespace LegalTrucking.IntakePlus.Core.Domain.Agents
+{
+    public class Agent : AggregateRoot
+    {
+        public Agent(String firstName, String lastName): base(Guid.NewGuid())
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public String FirstName { get; internal set; }
+        public String LastName { get; internal set; }
+    }
+}
