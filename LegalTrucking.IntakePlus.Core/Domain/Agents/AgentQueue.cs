250b4cb35d26228b7db8b86514c48f00f9db41a2
atillman34817 <adrian.tillman@gmail.com>
2019-04-18 15:16:22 -0400

    Automatically grow AgentQueue when it reaches capacity

--- LegalTrucking.IntakePlus.Core/Domain/Agents/AgentQueue.cs
+++ LegalTrucking.IntakePlus.Core/Domain/Agents/AgentQueue.cs
@@ -11,19 +11,42 @@ namespace LegalTrucking.IntakePlus.Core.Domain.Agents
     {
         private CircularBuffer<Agent> _buffer;
 
-        public AgentQueue()
+        public AgentQueue(int capacity)
         {
-            _buffer = new CircularBuffer<Agent>(10);
+            _buffer = new CircularBuffer<Agent>(capacity);
         }
 
         public void AddAn(Agent agent)
         {
+            if (this.SizeOf() == _buffer.Capacity)
+                IncreaseBufferCapacity();
+
             _buffer.PushBack(agent);
         }
 
         public bool Contains(Agent agent)
         {
             return _buffer.Contains(agent);
+        }
+
+        public int SizeOf()
+        {
+            return this._buffer.Size;
+        }
+
+        private void IncreaseBufferCapacity()
+        {
+            var newBuffer = new CircularBuffer<Agent>(_buffer.Size + 1);
+            Copy(_buffer, newBuffer);
+            this._buffer = newBuffer;
+        }
+
+        private void Copy(CircularBuffer<Agent> from, CircularBuffer<Agent> to)
+        {
+            foreach(var agent in from)
+            {
+                to.PushBack(agent);
+            }
         }
     }
 }
--- LegalTrucking.Tests.Unit/Domain/Agents/AgentQueueTests.cs
+++ LegalTrucking.Tests.Unit/Domain/Agents/AgentQueueTests.cs
@@ -10,6 +10,7 @@ using Machine.Specifications;
 
 namespace LegalTrucking.Tests.Unit.Domain.Agents
 {
+    [Subject(typeof(AgentQueue))]
     public class When_we_add_an_agent_they_get_added_to_the_queue
     {
         static AgentQueue _agentQueue;
@@ -18,11 +19,29 @@ namespace LegalTrucking.Tests.Unit.Domain.Agents
 
         Establish context = () =>
         {
-            _agentQueue = new AgentQueue();
+            _agentQueue = new AgentQueue(1);
         };
 
         Because of = () => _agentQueue.AddAn(agent);
 
         It should_contain_that_agent = () => _agentQueue.Contains(agent).ShouldEqual(true);
+    }
+
+    [Subject(typeof(AgentQueue))]
+    public class When_we_add_an_agent_if_the_queue_is_full_it_grows{
+        static AgentQueue agentQueue;
+        static Agent adrian = new Agent("Adrian", "Tillman");
+        static Agent fred = new Agent("Fred", "Sandford");
+
+        Establish context = () =>
+        {
+            agentQueue = new AgentQueue(1);
+            agentQueue.AddAn(adrian);
+        };
+
+        Because of = () => agentQueue.AddAn(fred);
+
+        It should_have_increased_the_capacity_to_be_greater_than_original = () => agentQueue.SizeOf().ShouldEqual(2);
+
     }
 }
