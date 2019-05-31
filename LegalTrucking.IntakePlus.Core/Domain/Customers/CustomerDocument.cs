7ba7cb5838379f72addbe3ca3689c3aac360128d
atillman34817 <adrian.tillman@gmail.com>
2019-05-31 14:07:37 -0400

    Implementing CustomerDocument

--- LegalTrucking.IntakePlus.Core/Domain/Customers/CustomerDocument.cs
+++ LegalTrucking.IntakePlus.Core/Domain/Customers/CustomerDocument.cs
@@ -1,8 +1,29 @@
 ﻿using LegalTrucking.IntakePlus.Core.Adapters.Repositories;
+using System;
+using Version = LegalTrucking.IntakePlus.Core.Adapters.Repositories.Version;
 
 namespace LegalTrucking.IntakePlus.Core.Domain.Customers
 {
     public class CustomerDocument : IAmADocument
     {
+        public CustomerDocument(Id id, Version version, string name, Address billingAddress,
+           Address shippingAddress, Contact contact)
+        {
+            Id = id;
+            Version = version;
+            Name = name;
+            BillingAddress = billingAddress;
+            ShippingAddress = shippingAddress;
+            Contact = contact;
+        }
+
+        public CustomerDocument() { }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string BillingAddress { get; set; }
+        public string ShippingAddress { get; set; }
+        public string Contact { get; set; }
+        public int Version { get; set; }
     }
 }
